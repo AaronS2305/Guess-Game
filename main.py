6c236d9a5da3f4bed30af9d2c8e6e10eeb89c024
Aaron235 <79173907+AaronS2305@users.noreply.github.com>
2021-11-24 22:46:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+#Number Guesseing Game version 3
+#25/11/2021
+#Aaron Sharma
+
+#importing a module
+import time
+
+#welcome message
+sleep.(.75)
+print("Welcome to the Pap High Virus Simulator\n Please follow the prompts")
+
+country_name = []
+
+#Target variable to store a random target number for the user to guess
+target = random.randint(1, 100)
+#guess variable to store user's guessed number
+
+
+run_loop = True
+
+while run_loop:
+  guess = int(input("Please guess a number between 1-100"))
+
+  while not(guess >=1 and guess <= 100):
+    guess = int(input("That is not a valid entry, Please enter a number between 1-100"))
+
+
+
+  if guess < target:
+   print("Your guess was too low")
+  elif guess > target:
+    print("Your guess was too high")
+  elif guess == target:
+    run_loop = False
+
+
+
+print("Congratulations, you guessed correctly")
